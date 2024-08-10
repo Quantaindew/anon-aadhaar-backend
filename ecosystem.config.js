270703144141e08ace7272f88d5046be18f5a29d
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-08-10 23:06:53 +0530

    upload ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps : [{
+    name: "anon-aadhaar-backend",
+    script: "dist/index.js",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '2G',
+    node_args: "--expose-gc --max-old-space-size=2048",
+    env: {
+      NODE_ENV: "production",
+    },
+  }],
+
+  deploy : {
+    production : {
+      user : 'paperspace',
+      host : '184.105.118.135',
+      ref  : 'origin/main',
+      repo : 'https://github.com/Quantaindew/anon-aadhaar-backend.git',
+      path : '/home/paperspace/anon-aadhaar-backend',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': 'npm install -g pm2'
+    }
+  }
+};
