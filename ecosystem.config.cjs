3e4ad30ae6198b6305e694dc4bc10e2dbf95b631
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-08-11 22:31:22 +0530

    blow it up

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,13 +2,21 @@ module.exports = {
   apps : [{
     name: "anon-aadhaar-backend",
     script: "dist/index.js",
-    instances: 1,
+    instances: 4,
+    exec_mode: "cluster",
     autorestart: true,
     watch: false,
-    max_memory_restart: '2G',
-    node_args: "--expose-gc",
+    max_memory_restart: '3.5G',
+    node_args: "--max-old-space-size=3584 --expose-gc",
     env: {
       NODE_ENV: "production",
     },
+    env_production: {
+      NODE_ENV: "production",
+    },
+    exp_backoff_restart_delay: 100,
+    merge_logs: true,
+    log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+    kill_timeout : 3000,
   }],
 };
