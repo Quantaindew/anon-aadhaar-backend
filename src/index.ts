39688478ae34aa681d085fb67367d20cdf11776c
Quantaindew <35188255+Quantaindew@users.noreply.github.com>
2024-08-11 20:23:55 +0530

    asdasd

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ const logMemoryUsage = () => {
 dotenv.config();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // Add these lines to set global timeout and increase payload limit
 app.use((req, res, next) => {
